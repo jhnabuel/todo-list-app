8307ce884efb7dd28799805d2986360816bc4c99
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-03-21 00:40:36 +0800

    Feature: started Auth functionality and finished AuthProvider.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { useContext, useState, createContext, useEffect } from "react";
+import supabase from "../utils/supabaseClient";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [session, setSession] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const { data: { subscription } } = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                setSession(session);
+                setUser(session?.user ?? null);
+                setLoading(false);
+            }
+        );
+        return () => subscription.unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ session, user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
