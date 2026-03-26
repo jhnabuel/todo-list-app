77791ad39274d6119ae058d4ebff9ed84797f32b
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-03-27 00:41:32 +0800

    chore(routing): fixed routing for login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import Login from './pages/auth/Login'
 import Signup from './pages/auth/Signup'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
