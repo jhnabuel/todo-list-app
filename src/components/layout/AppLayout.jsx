05e3212dd34729b18194df02cd0703455f347f1f
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-04-10 10:26:06 +0800

    feat(frontend): Finished topbar and app layout structure.

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,17 @@
 import Sidebar from "./Sidebar"
+import Topbar from "./Topbar"
 
-export default function AppLayout() {
+export default function AppLayout({ children }) {
     return (<>
 
         <div className="flex h-screen overflow-hidden bg-stone-50">
             <Sidebar />
+            <div className="flex flex-col flex-1 overflow-hidden">
+                <Topbar />
+                <main className="flex-1 overflow-y-auto p-6">
+                    {children}
+                </main>
+            </div>
         </div>
 
     </>)
