c267880996f4312b25e5294c464156a83b33429c
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-04-15 16:44:20 +0800

    feat(frontend): Created TaskItem component

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { useTasks } from "../../hooks/useTasks";
 import AppLayout from "../../components/layout/AppLayout";
+import TaskItem from "../../components/tasks/TaskItem";
 
 export default function DashBoard() {
 
@@ -9,6 +10,20 @@ export default function DashBoard() {
         <>
             <AppLayout>
                 <p className="text-sm text-stone-400 font-mono">Tasks will appear here.</p>
+
+                <TaskItem
+                    task={{
+                        id: '1',
+                        title: 'Test task',
+                        status: 'todo',
+                        priority: 'high',
+                        due_date: '2024-12-20'
+                    }}
+                    onEdit={() => console.log('edit')}
+                    onDelete={() => console.log('delete')}
+                    onToggle={() => console.log('toggle')}
+                    isLast={true}
+                />
             </AppLayout>
 
         </>
