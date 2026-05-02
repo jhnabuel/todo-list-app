90da3dcf3202dac38ee21501367d03a13adda1a2
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-05-02 23:20:43 +0800

    feat(data): Users can now add tasks using the modal

--- src/components/tasks/TaskModal.jsx
+++ src/components/tasks/TaskModal.jsx
@@ -73,7 +73,7 @@ export default function TaskModal({ isOpen, onClose, onSubmit, task }) {
                                 >
                                     <option value="todo">To-Do</option>
                                     <option value="in_progress">In Progress</option>
-                                    <option value="done">Done</option>
+                                    <option value="completed">Done</option>
                                 </select>
                             </div>
 
