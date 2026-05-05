4b2318ac5fd84fb7c3a9344000659752d819078e
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-05-05 13:15:28 +0800

    feat(frontend): Refractored dashboard and component structure

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,15 +3,27 @@ import { useTasks } from "../../hooks/useTasks";
 import AppLayout from "../../components/layout/AppLayout";
 import TaskList from "../../components/tasks/TaskList";
 import TaskModal from "../../components/tasks/TaskModal";
+
 export default function DashBoard() {
     const [modalOpen, setModalOpen] = useState(false);
     const { tasks, loading, saveTask, removeTask } = useTasks();
     const [editingTask, setEditingTask] = useState(null);
 
-    const handleEdit = (task) => {
+    const openModal = (task = null) => {
         setEditingTask(task);
         setModalOpen(true);
-    };
+    }
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setEditingTask(null);
+    }
+
+    const handleModalSubmit = async (formData) => {
+        await saveTask(formData, editingTask);
+        closeModal();
+        setSelectedTask(null);
+    }
 
     const handleDelete = async (id) => {
         if (!window.confirm('Are you sure you want to delete this task?')) return;
@@ -19,21 +31,29 @@ export default function DashBoard() {
     }
 
     const handleToggle = async (id, currentStatus) => {
-        await updateTask(id, {     // calls the hook function
+        await saveTask({
             status: currentStatus === 'done' ? 'todo' : 'done'
-        });
+        }, { id });
     };
 
 
     return (
         <>
-            <TaskList
-                tasks={tasks}
-                loading={loading}
-                onEdit={handleEdit}
-                onDelete={handleDelete}
-                onToggle={handleToggle}
-            />
+            <AppLayout onNewTask={() => openModal()}>
+                <TaskList
+                    tasks={tasks}
+                    loading={loading}
+                    onEdit={openModal}
+                    onDelete={handleDelete}
+                    onToggle={handleToggle}
+                />
+
+                <TaskModal
+                    isOpen={modalOpen}
+                    onClose={closeModal}
+                    onSubmit={handleModalSubmit}
+                    selectedTask={editingTask} />
+            </AppLayout>
         </>
     )
 }
